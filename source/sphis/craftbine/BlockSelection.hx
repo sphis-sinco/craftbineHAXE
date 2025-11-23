13e3d43609ad776be61b030d68223a35e8f31cb4
Viwu <sinconsistencia@gmail.com>
2025-11-23 15:55:17 -0600

    test ended up in "no point now"

--- source/sphis/craftbine/BlockSelection.hx
+++ source/sphis/craftbine/BlockSelection.hx
@@ -22,7 +22,7 @@ class BlockSelection extends FlxSubState
 	{
 		super.update(elapsed);
 
-		if (FlxG.keys.justReleased.ESCAPE && !FlxG.keys.pressed.SHIFT)
+		if (FlxG.keys.justReleased.ESCAPE)
 			close();
 	}
 }
