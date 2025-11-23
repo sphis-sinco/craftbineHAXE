216f692f2ad14ea3fa21f7627c4a13506d16ffe8
Viwu <sinconsistencia@gmail.com>
2025-11-23 16:01:36 -0600

    block selection iter 1

--- source/sphis/craftbine/BlockSelection.hx
+++ source/sphis/craftbine/BlockSelection.hx
@@ -1,5 +1,7 @@
 package sphis.craftbine;
 
+import flixel.math.FlxMath;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxSubState;
@@ -16,13 +18,63 @@ class BlockSelection extends FlxSubState
 		bg.makeGraphic(FlxG.width, FlxG.height);
 		bg.alpha = .5;
 		add(bg);
+
+		blocks = new FlxTypedGroup<Block>();
+		add(blocks);
+
+		var x = 0;
+		var y = 0;
+        selected_block = Block.BLOCK_LIST[0];
+		for (block in Block.BLOCK_LIST)
+		{
+			var nb = new Block(block, x * 20, y * 20, block ->
+			{
+				block.hsv_shader.value = 0.5;
+                selected_block = block.block_id;
+			});
+
+			x++;
+            //                                            16.5 won't be 17, it'll be 16. I hope anyway.
+			if (x > FlxMath.roundDecimal(FlxG.width / 20, 0))
+			{
+				x = 0;
+				y++;
+			}
+		}
 	}
+
+	var blocks:FlxTypedGroup<Block>;
+    var selected_block:String = 'grass_block';
+    var selected_ID:Int = 0;
 
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
+        if (FlxG.keys.anyJustReleased([LEFT, A, RIGHT, D]))
+		{
+			if (FlxG.keys.anyJustReleased([LEFT, A]))
+			{
+				selected_ID--;
+			}
+			else
+			{
+				selected_ID++;
+			}
+
+			if (selected_ID < 0)
+				selected_ID = 0;
+
+			if (selected_ID > blocks.length)
+				selected_ID--;
+
+			for (block in blocks.members)
+				block.selected = selected_ID == block.ID;
+		}
+
 		if (FlxG.keys.justReleased.ESCAPE)
+		{
 			close();
+		}
 	}
 }
