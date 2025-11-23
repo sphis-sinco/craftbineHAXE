142d04281f2bb769206c81860d3d9b666f897070
Viwu <sinconsistencia@gmail.com>
2025-11-23 12:12:07 -0600

    pixel perfect rendering on web

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,12 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+
+		#if web
+		// pixel perfect render fix!
+		lime.app.Application.current.window.element.style.setProperty("image-rendering", "pixelated");
+		#end
+
 		addChild(new FlxGame(0, 0, PlayState));
 	}
 }
