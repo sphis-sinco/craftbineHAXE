0abdc76d92979a19480cf80e2d939ec9fb458242
Viwu <sinconsistencia@gmail.com>
2025-11-23 12:08:42 -0600

    +=

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -11,13 +11,14 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
+		
 		grass_block = new Block().setIconIndex(0);
 		stone_block = new Block().setIconIndex(1);
 
 		grass_block.screenCenter();
 		stone_block.screenCenter();
 		grass_block.x -= grass_block.width;
-		stone_block.x -= stone_block.width;
+		stone_block.x += stone_block.width;
 
 		add(grass_block);
 		add(stone_block);
